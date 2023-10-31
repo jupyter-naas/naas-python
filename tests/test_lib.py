bc0ddb9107d70d719ddc786371cfdecbb60a69ef
vinicius douglas cerutti <vinivdc2009@hotmail.com>
2023-10-31 13:57:35 -0300

    make tests pass

--- tests/test_lib.py
+++ tests/test_lib.py
@@ -1,3 +1,18 @@
-def test_lib():
+import pytest
+
+
+def test_lib_add_import():
     import naas_python as naas
-    naas.space.add()
+
+    # Test if ``naas.space.add`` is a valid method and callable
+
+    assert callable(naas.space.add)
+
+
+def test_missing_keys_call():
+    import naas_python as naas
+
+    # Test if ``naas.space.add`` is a valid method and callable
+
+    with pytest.raises(TypeError):
+        naas.space.add()
