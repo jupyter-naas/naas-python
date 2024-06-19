63ed369278efd3eda174871b9e7b1abacef78dd5
Florent <48032461+FlorentLvr@users.noreply.github.com>
2024-06-19 14:43:09 +0200

    fix: rename domain asset function

--- naas_python/domains/asset/AssetDomain.py
+++ naas_python/domains/asset/AssetDomain.py
@@ -14,19 +14,19 @@ class AssetDomain(IAssetDomain):
     def __init__(self, adaptor: IAssetAdaptor):
         self.adaptor = adaptor
 
-    def create_asset(self, workspace_id:str, asset_creation:AssetCreation) -> Asset:
-        asset = self.adaptor.create_asset(workspace_id, asset_creation)
+    def create(self, workspace_id:str, asset_creation:AssetCreation) -> Asset:
+        asset = self.adaptor.create(workspace_id, asset_creation)
         return asset
     
-    def get_asset(self, workspace_id:str, asset_id:str) -> Asset:
-        asset = self.adaptor.get_asset(workspace_id, asset_id)
+    def get(self, workspace_id:str, asset_id:str) -> Asset:
+        asset = self.adaptor.get(workspace_id, asset_id)
         return asset
 
 
-    def update_asset(self, workspace_id:str, asset_id:str, asset_update: AssetUpdate) -> Asset:
-        response = self.adaptor.update_asset(workspace_id, asset_id, asset_update)
+    def update(self, workspace_id:str, asset_id:str, asset_update: AssetUpdate) -> Asset:
+        response = self.adaptor.update(workspace_id, asset_id, asset_update)
         return response
 
-    def delete_asset(self, workspace_id:str, asset_id:str) -> None:
-        self.adaptor.delete_asset(workspace_id, asset_id)
+    def delete(self, workspace_id:str, asset_id:str) -> None:
+        self.adaptor.delete(workspace_id, asset_id)
         return None
