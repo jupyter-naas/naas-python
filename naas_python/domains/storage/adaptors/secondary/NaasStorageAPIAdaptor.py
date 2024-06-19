31d2474e81f112519597557cafc16a6471466745
Maxime Jublou <jubloum@gmail.com>
2024-06-19 15:41:39 +0200

    fix: Generate credentials

--- naas_python/domains/storage/adaptors/secondary/NaasStorageAPIAdaptor.py
+++ naas_python/domains/storage/adaptors/secondary/NaasStorageAPIAdaptor.py
@@ -107,7 +107,7 @@ class NaasStorageAPIAdaptor(BaseAPIAdaptor, IStorageAdaptor):
 
     def generate_credentials(self, workspace_id :str, storage_name: str) -> dict:
 
-        _url = f"{self.host}/workspace/{workspace_id}/storage/credentials"
+        _url = f"{self.host}/workspace/{workspace_id}/storage/credentials/"
 
         api_response = self.make_api_request(
             requests.post,
