74e4c49c3646129cdc560d018bb5194aa2725090
Florent <48032461+FlorentLvr@users.noreply.github.com>
2024-06-25 10:50:43 +0200

    fix: update AssetDomain.py

--- naas_python/domains/asset/AssetDomain.py
+++ naas_python/domains/asset/AssetDomain.py
@@ -14,19 +14,19 @@ class AssetDomain(IAssetDomain):
     def __init__(self, adaptor: IAssetAdaptor):
         self.adaptor = adaptor
 
-    def create_asset(self, workspace_id:str, asset_creation:AssetCreation) -> Asset:
+    def create(self, workspace_id:str, asset_creation:AssetCreation) -> Asset:
         asset = self.adaptor.create_asset(workspace_id, asset_creation)
         return asset
     
-    def get_asset(self, workspace_id:str, asset_id:str) -> Asset:
+    def get(self, workspace_id:str, asset_id:str) -> Asset:
         asset = self.adaptor.get_asset(workspace_id, asset_id)
         return asset
 
 
-    def update_asset(self, workspace_id:str, asset_id:str, asset_update: AssetUpdate) -> Asset:
+    def update(self, workspace_id:str, asset_id:str, asset_update: AssetUpdate) -> Asset:
         response = self.adaptor.update_asset(workspace_id, asset_id, asset_update)
         return response
 
-    def delete_asset(self, workspace_id:str, asset_id:str) -> None:
+    def delete(self, workspace_id:str, asset_id:str) -> None:
         self.adaptor.delete_asset(workspace_id, asset_id)
         return None
